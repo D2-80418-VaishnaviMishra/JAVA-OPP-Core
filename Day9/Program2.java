794f3d1a6fb790ba2e0c1547f403b63301823628
VaishnaviMishra <vaishnavim424@gmail.com>
2023-10-30 18:20:38 +0530

    Assignment

---
+++ Day9/Program2.java
@@ -0,0 +1,23 @@
+package Assignment;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		/*
+		 * String str = "Bank Of Maharashtra"; String str1 = str.toLowerCase();
+		 * System.out.println(str1);
+		 * 
+		 * String[] str2 = str.split(" "); System.out.println();
+		 */
+		
+		String s = "this is string" ; 
+		String s1 = s.toUpperCase();
+		System.out.println(s1);
+		s1.s
+		
+		
+		
+	}
+
+}
+
